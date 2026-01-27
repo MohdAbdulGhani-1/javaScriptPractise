2bee0b2828755397d57636bb67b5bf6926b6a018
Mohd Abdul Ghani <mohdabdulghani177@gmail.com>
2026-01-27 15:19:30 +0530

    day 15 notes done

---
+++ javascripets/day15/notes15.js
@@ -0,0 +1,21 @@
+// Context Example
+var n = 2;
+
+function square(num) {
+  var ans = num * num;
+  return ans;
+}
+
+var square2 = square(n);
+var square4 = square(4);
+
+// Multiple Function Calls 
+
+function square(num) {
+  var ans = num * num;
+  return ans;
+}
+
+var square2 = square(n);
+var square4 = square(4);
+
